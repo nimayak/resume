69ab092f28f879200b79cad97d1500c280339a4d
nimayaK <nimaya.kodikara@gmail.com>
2019-02-22 20:12:04 +1100

    add moore image icons to portfolio

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -5,7 +5,10 @@ export default class Portfolio extends Component {
       <React.Fragment>
       {/* Portfolio Section
    ================================================== */}
+
       <section id="portfolio">
+    
+
         <div className="row">
           <div className="twelve columns collapsed">
             <h1>Check Out Some of My Work.</h1>
@@ -101,8 +104,8 @@ export default class Portfolio extends Component {
                     <img alt src="images/portfolio/HRAFF-website.png" />
                     <div className="overlay">
                       <div className="portfolio-item-meta">
-                        <h5>Origami</h5>
-                        <p>Illustration</p>
+                        <h5>Website Redesign for HRAFF</h5>
+                        <p>Web design</p>
                       </div>
                     </div>
                     <div className="link-icon"><i className="icon-plus" /></div>
@@ -112,11 +115,123 @@ export default class Portfolio extends Component {
               <div className="columns portfolio-item">
                 <div className="item-wrap">
                   <a href="#modal-08" title>
-                    <img alt src="images/portfolio/retrocam.jpg" />
+                    <img alt src="images/portfolio/vintage-collectibles.png" />
                     <div className="overlay">
                       <div className="portfolio-item-meta">
-                        <h5>Retrocam</h5>
-                        <p>Web Development</p>
+                        <h5>Vintage Store Website</h5>
+                        <p>Web Design</p>
+                      </div>
+                    </div>
+                    <div className="link-icon"><i className="icon-plus" /></div>
+                  </a>
+                </div>
+              </div>  {/* item end */}
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-09" title>
+                    <img alt src="images/portfolio/task-app.png" />
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>Tasks app</h5>
+                        <p>Web App</p>
+                      </div>
+                    </div>
+                    <div className="link-icon"><i className="icon-plus" /></div>
+                  </a>
+                </div>
+              </div> {/* item end */}
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-10" title>
+                    <img alt src="images/portfolio/weather-app.png" />
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>Logo</h5>
+                        <p>Graphic Design</p>
+                      </div>
+                    </div>
+                    <div className="link-icon"><i className="icon-plus" /></div>
+                  </a>
+                </div>
+              </div> {/* item end */}
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-11" title>
+                    <img alt src="images/portfolio/CoverRedesign2.png" />
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>Book Cover Redesign</h5>
+                        <p>Graphic Design</p>
+                      </div>
+                    </div>
+                    <div className="link-icon"><i className="icon-plus" /></div>
+                  </a>
+                </div>
+              </div> {/* item end */}
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-12" title>
+                    <img alt src="images/portfolio/CoverRedesign3.png" />
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>Book Cover Redesign</h5>
+                        <p>Graphic Design</p>
+                      </div>
+                    </div>
+                    <div className="link-icon"><i className="icon-plus" /></div>
+                  </a>
+                </div>
+              </div> {/* item end */}
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-13" title>
+                    <img alt src="images/portfolio/drawing-app.png" />
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>Children's Drawing App</h5>
+                        <p>HTML5 Canvas</p>
+                      </div>
+                    </div>
+                    <div className="link-icon"><i className="icon-plus" /></div>
+                  </a>
+                </div>
+              </div> {/* item end */}
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-14" title>
+                    <img alt src="images/portfolio/band-website.png" />
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>Band Website</h5>
+                        <p>Web Design</p>
+                      </div>
+                    </div>
+                    <div className="link-icon"><i className="icon-plus" /></div>
+                  </a>
+                </div>
+              </div> {/* item end */}
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-15" title>
+                    <img alt src="images/portfolio/HRAFF-website.png" />
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>Website Redesign for HRAFF</h5>
+                        <p>Web design</p>
+                      </div>
+                    </div>
+                    <div className="link-icon"><i className="icon-plus" /></div>
+                  </a>
+                </div>
+              </div> {/* item end */}
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-16" title>
+                    <img alt src="images/portfolio/task-app.png" />
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>Vintage Store Website</h5>
+                        <p>Web Design</p>
                       </div>
                     </div>
                     <div className="link-icon"><i className="icon-plus" /></div>
@@ -140,7 +255,7 @@ export default class Portfolio extends Component {
             </div>
           </div>{/* modal-01 End */}
           <div id="modal-02" className="popup-modal mfp-hide">
-            <img className="scale-with-grid" src="images/portfolio/modals/m-console.jpg" alt />
+            <img className="scale-with-grid" src="images/portfolio/modals/OldSpiceLogo.png" alt />
             <div className="description-box">
               <h4>Console</h4>
               <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
@@ -152,7 +267,7 @@ export default class Portfolio extends Component {
             </div>
           </div>{/* modal-02 End */}
           <div id="modal-03" className="popup-modal mfp-hide">
-            <img className="scale-with-grid" src="images/portfolio/modals/m-judah.jpg" alt />
+            <img className="scale-with-grid" src="images/portfolio/modals/CoverRedesign2.png" alt />
             <div className="description-box">
               <h4>Judah</h4>
               <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
@@ -164,7 +279,7 @@ export default class Portfolio extends Component {
             </div>
           </div>{/* modal-03 End */}
           <div id="modal-04" className="popup-modal mfp-hide">
-            <img className="scale-with-grid" src="images/portfolio/modals/m-intothelight.jpg" alt />
+            <img className="scale-with-grid" src="images/portfolio/modals/CoverRedesign3.png" alt />
             <div className="description-box">
               <h4>Into the Light</h4>
               <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
@@ -188,7 +303,7 @@ export default class Portfolio extends Component {
             </div>
           </div>{/* modal-05 End */}
           <div id="modal-06" className="popup-modal mfp-hide">
-            <img className="scale-with-grid" src="images/portfolio/modals/m-girl.jpg" alt />
+            <img className="scale-with-grid" src="images/portfolio/modals/band-website.png" alt />
             <div className="description-box">
               <h4>Girl</h4>
               <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
@@ -200,7 +315,7 @@ export default class Portfolio extends Component {
             </div>
           </div>{/* modal-06 End */}
           <div id="modal-07" className="popup-modal mfp-hide">
-            <img className="scale-with-grid" src="images/portfolio/modals/m-origami.jpg" alt />
+            <img className="scale-with-grid" src="images/portfolio/modals/HRAFF-website.png" alt />
             <div className="description-box">
               <h4>Origami</h4>
               <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
@@ -212,7 +327,7 @@ export default class Portfolio extends Component {
             </div>
           </div>{/* modal-07 End */}
           <div id="modal-08" className="popup-modal mfp-hide">
-            <img className="scale-with-grid" src="images/portfolio/modals/m-retrocam.jpg" alt />
+            <img className="scale-with-grid" src="images/portfolio/modals/vintage-collectibles.png" alt />
             <div className="description-box">
               <h4>Retrocam</h4>
               <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
